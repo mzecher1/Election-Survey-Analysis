acb3b4e20854b1e138b6dc551b99c55870cf5cf0
Maxwell Austensen <maxwell.austensen@nyu.edu>
2020-06-28 16:08:33 -0400

    fix typo

--- unzip-anes-data.R
+++ unzip-anes-data.R
@@ -1,7 +1,7 @@
 library(zip)
 library(fs)
 
-# If the .data file doesn't already exist, then unzip to create it
+# If the .dta file doesn't already exist, then unzip to create it
 
 if (!file_exists(path("data-raw", "anes_timeseries_cdf_stata13.dta"))) {
   unzip(
